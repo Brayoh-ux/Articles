60c39da723dc8cccfb965f68009dd14553497a1e
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-02 10:34:19 +0300

    give min value of password

--- blog/forms.py
+++ blog/forms.py
@@ -5,8 +5,8 @@ from wtforms.validators import Required, Length, Email,EqualTo
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[Required(), Length(min =2, max =20)])
     email = StringField('Email', validators=[Required(), Email() ])
-    password = PasswordField('Password', validators=[Required(), Length(min =8, max=16)])
-    password_confirm = PasswordField('Confirm Password', validators=[Required(), EqualTo('password'), Length(min =8, max=16)])
+    password = PasswordField('Password', validators=[Required(), Length(min =8)])
+    password_confirm = PasswordField('Confirm Password', validators=[Required(), EqualTo('password'), Length(min =8)])
     submit =SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
