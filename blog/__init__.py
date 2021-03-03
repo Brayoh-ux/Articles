c5f5745c05657fdff778c83e51151ad5f3110ac3
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-03 13:13:45 +0300

    initialize database

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,11 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
+
+
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'vnsjddh4ckmvttl6pvcllxty6add'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tuwdmxxnafbflg:d59552a7759b49ceb405196eff30c56ad47c34ef0762bd64f0a56174ab9f7922@ec2-18-211-97-89.compute-1.amazonaws.com:5432/def9bqfmrk6r4u'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://moringaaccess:5693@localhost/myblog'
+
 db = SQLAlchemy(app)
+
+db.init_app(app)
 
 from blog import routes
 
