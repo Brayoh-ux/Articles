57d715602afd2a3555507c8a9b716e650bc1120a
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-08 01:06:01 +0300

    register route

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from flask import render_template, flash, url_for, redirect
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, url_for, redirect, request
+from app import app, db
+from app.forms import LoginForm, RegisterForm
+from flask_login import login_required,  current_user, login_user, logout_user
+from app.models import User
 
 
 @app.route('/')
@@ -24,7 +26,70 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        flash('Login Succefull!')
-        return redirect( url_for('index'))
+
+        user = User.query.filter_by(email=form.email.data).first()
+        
+        if user is None or not user.check_password(form.password.data):
+            
+            flash('Invalid email or password')
+            
+            return redirect(url_for('login'))
+        
+        login_user(user, remember=form.remember_me.data)
+        
+        next_page = request.args.get('next')
+        
+        if not next_page or url_parse(next_page).netloc != '':
+          
+            next_page = url_for('blog')
+        
+        return redirect(next_page)
 
     return render_template('login.html', title = 'Sign in', form = form)
+
+
+@app.route('/blog')
+@login_required
+def blog():
+
+    pitches = [
+        {
+            'author': { 'username' : 'John' },
+            'body' : 'My new pitch'
+        }
+    ]
+
+    return render_template('blog.html', pitches = pitches)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/comment')
+@login_required
+def comment():
+
+    return render_template('comment.html')
+
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+
+    if current_user.is_authenticated:
+        return redirect (url_for('index'))
+
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username = form.username.data, email = form.email.data)
+        user.set_password(form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Regestration successfull!')
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title = 'Register', form = form)
