32b9f8e92420d78e70cb0c00e7d08f5276429295
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-08 01:07:05 +0300

    add shell context

--- manage.py
+++ manage.py
@@ -1,4 +1,10 @@
-from app import app
+from app import app, db
+from app.models import User,Pitches
+
+@app.shell_context_processor
+def make_shell_context():
+
+    return {'db': db, 'User': User, 'Pitches' : Pitches}
 
 
 
