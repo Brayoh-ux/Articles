45e59a0601bc69c4a355069c2076c36af7309f30
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-07 22:55:05 +0300

    add login route

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, url_for, redirect
 from app import app
-
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -16,3 +16,15 @@ def index():
     ]
 
     return render_template('home.html', user = user, pitches = pitches)
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login Succefull!')
+        return redirect( url_for('index'))
+
+    return render_template('login.html', title = 'Sign in', form = form)
