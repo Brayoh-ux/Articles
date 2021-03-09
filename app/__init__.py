b7001acd2c883491cd5e75614be461a2fe31f3fd
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-09 16:35:38 +0300

    deployment to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from config import Config
+from config import Config, DevConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -9,6 +9,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 
 app.config.from_object(Config)
+app.config.from_object(DevConfig)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- config.py
+++ config.py
@@ -8,3 +8,7 @@ class Config():
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevConfig(Config):
+    DEBUG = True
